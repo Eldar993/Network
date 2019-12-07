0a05a72557a22fa40465e910e329256572b23b0c
Eldar <eldar7989@gmail.com>
2019-12-07 15:15:59 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int computers = scanner.nextInt();
+    double distance = 0;
+    int[][] points = new int[][];
+    boolean point = true;
+    for(int  i = 1;i <= computers;i++){
+        for (int  j = 1;j <=computers) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+        int[x][y] = point;
+        }
+    }
+
+    // BFS...
+
+
+        System.out.println(distance);
+    }
+}
